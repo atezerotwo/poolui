86a95a56664214a78c651cd779af9b117ce78a0e
802 <atezerotwo@gmail.com>
2017-09-22 10:02:36 +0100

    added coin chain url

--- app/globals.default.js
+++ app/globals.default.js
@@ -6,6 +6,7 @@ angular.module('pool.globals', [])
 	return {
 		coin_code: "SUMO",
 		coin_div: "1000000000", //1e9
+		coin_chain_url: "https://explorer.sumokoin.com",
 		pool_name: "alt_nodejs_pool",
 		api_url : 'http://publicipofyourserver',
 		api_refresh_interval: 5000,
@@ -19,12 +20,19 @@ coins available are:
 
 coin_code: "XMR",
 coin_div: "100000000000" //1e12
+coin_chain_url: "https://xmrchain.net",
 
 coin_code: "KRB",
 coin_div: "1000000000000" //1e12
+coin_chain_url: "https://explorer.karbowanec.com/en",
 
 coin_code: "SUMO",
 coin_div: "1000000000" //1e9
-
+coin_chain_url: "https://explorer.sumokoin.com",
+		
 coin_code: "BCN",
 coin_div: "1000000000" //1e9
+coin_chain_url: "https://minergate.com/blockchain/bcn",
+
+*/
+
