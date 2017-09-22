90b2d77357d0e02905ff3c51f590c88cff1cf32a
802 <atezerotwo@gmail.com>
2017-09-22 09:49:59 +0100

    added alt coin_div

--- app/globals.js
+++ app/globals.js
@@ -5,9 +5,29 @@ angular.module('pool.globals', [])
 .factory('GLOBALS', function() {
 	return {
 		coin_code: "SUMO",
-		pool_name: "nodejspool",
-		api_url : 'https://api.xmrpool.net',
+		coin_div: "1000000000", //1e9
+		pool_name: "alt_nodejs_pool",
+		api_url : 'http://publicipofyourserver',
 		api_refresh_interval: 5000,
 		app_update_interval: 5*60000
 	};
 });
+
+/*
+
+coins available are:
+
+coin_code: "XMR",
+coin_div: "100000000000" //1e12
+
+coin_code: "KRB",
+coin_div: "1000000000000" //1e12
+
+coin_code: "SUMO",
+coin_div: "1000000000" //1e9
+
+coin_code: "BCN",
+coin_div: "1000000000" //1e9
+
+*/
+
